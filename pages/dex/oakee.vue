e1186ee150969a4bc5500b9b9ebe074ba8061652
Oak Soe Khant <6131305048@lamduan.mfu.ac.th>
2020-10-14 22:22:23 +0700

    init

--- pages/dex/oakee.vue
+++ pages/dex/oakee.vue
@@ -1,23 +1,50 @@
 <template>
-  <div>
-    <H2>My Pokedex Content</H2>
-    <v-toolbar>
-      <v-text-field v-model="search" label="Search" />
+  <v-container>
+    <v-toolbar flat>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="Search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-spacer></v-spacer>
     </v-toolbar>
     <v-layout wrap>
-      <v-flex lg2 md3 sm6 xs12 v-for="(pk, idx) in this.filterDex" :key="idx">
-        <v-card outlined>
-          <v-img
-            :src="pk.sprites.animated"
-            style="padding: 20px"
-            width="150px"
-            height="100px"
-          />
-          <v-title>{{ pk.name }}</v-title>
-        </v-card>
+      <v-flex
+        pa-1
+        lg2
+        md3
+        sm6
+        xs12
+        v-for="(pk, idx) in this.filterDex"
+        :key="idx"
+      >
+        <v-fade-transition mode="out-in">
+          <v-row>
+            <v-col cols="10">
+              <v-card>
+                <v-img
+                  :src="pk.sprites.large"
+                  style
+                  max-height="100"
+                  max-width="120"
+                  contain
+                  class="grey darken-4"
+                ></v-img>
+                <v-card-title>{{ pk.name }}</v-card-title>
+                <v-card-subtitle>{{ pk.type }}</v-card-subtitle>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-icon @click="addFav(pk)">FAV</v-icon>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-fade-transition>
       </v-flex>
     </v-layout>
-  </div>
+  </v-container>
 </template>
 <script>
 const URL =
